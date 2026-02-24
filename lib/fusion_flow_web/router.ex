f3335f68dae792afc1627130f5c1aed9d15df908
Pedro Henrique <69769760+pedrohfonseca81@users.noreply.github.com>
2026-02-24 17:48:29 -0300

    Revert "Feature/users"

--- lib/fusion_flow_web/router.ex
+++ lib/fusion_flow_web/router.ex
@@ -19,11 +19,6 @@ defmodule FusionFlowWeb.Router do
     plug :accepts, ["json"]
   end
 
-  # Guest-only: redirect authenticated users to /
-  pipeline :guest_only do
-    plug :redirect_if_authenticated
-  end
-
   # JSON API Scope
   scope "/api", FusionFlowWeb do
     pipe_through :api
@@ -55,7 +50,6 @@ defmodule FusionFlowWeb.Router do
     live_session :require_authenticated_user,
       on_mount: [{FusionFlowWeb.UserAuth, :require_authenticated}] do
       live "/users/settings", UserLive.Settings, :edit
-      live "/users", UserLive.Index
       live "/", DashboardLive
       live "/flows", FlowListLive
       live "/flows/new/ai", FlowAiCreatorLive
@@ -63,19 +57,18 @@ defmodule FusionFlowWeb.Router do
     end
 
     post "/users/update-password", UserSessionController, :update_password
-    delete "/users/log-out", UserSessionController, :delete
   end
 
   scope "/", FusionFlowWeb do
-    pipe_through [:browser, :guest_only]
+    pipe_through [:browser]
 
     live_session :current_user,
       on_mount: [{FusionFlowWeb.UserAuth, :mount_current_scope}] do
       live "/setup", UserLive.Setup, :new
-      live "/users/invite/:token", UserLive.InviteRegister, :new
       live "/users/log-in", UserLive.Login, :new
     end
 
     post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
